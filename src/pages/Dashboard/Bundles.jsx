f181f4d3a3b181036e0816852a424df44463f947
Anandha Balu <balua2@coventry.ac.uk>
2022-10-06 15:10:08 +0100

    added dashboard pages

---
+++ src/pages/Dashboard/Bundles.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Bundles() {
+  return <div>Bundles</div>;
+}
+
+export default Bundles;
---
+++ src/pages/Dashboard/Cryptos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Cryptos() {
+  return <div>Cryptos</div>;
+}
+
+export default Cryptos;
